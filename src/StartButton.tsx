1083efb9e9f1b4390b682893ed31aa114a725a20
JennyWeij <jennyweij@outlook.com>
2023-02-26 14:20:50 +0100

    lagt till knappen

--- src/StartButton.tsx
+++ src/StartButton.tsx
@@ -2,15 +2,21 @@ import styled from "styled-components";
 
 function StartButton() {
   return (
-    <ButtonStart>Let's go!</ButtonStart>
+    <ButtonStart>
+      <p>
+        Let's go!        
+      </p>
+    </ButtonStart>
   )
 }
 
 const ButtonStart = styled.div`
 position: absolute;
-top: 70%;
-left: 40%;
-color: white;
+top: 75%;
+left: 45%;
+color: #6B0848;
+height: 5.5rem;
+width: 6.5rem;
 display: flex;
 flex-direction: column;
 justify-content: left;
@@ -18,6 +24,13 @@ align-items: left;
 text-align: left;
 padding: 2rem;
 font-size: 5rem;
+background-color: #59D8F4;
+border-radius: 80%;
+display: flex;
+flex-direction: column;
+padding: 2rem;
+font-size: 2.5rem;
+box-shadow: 0.2rem 0.2rem 0.2rem 0.2rem lightblue;
 `;
 
 
