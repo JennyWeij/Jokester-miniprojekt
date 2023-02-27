6460f6e15b31a64d6b9b597461679f99a10b0f5e
JennyWeij <jennyweij@outlook.com>
2023-02-27 10:13:00 +0100

    la till StartPage

---
+++ src/StartPage.tsx
@@ -0,0 +1,19 @@
+import "./assets/fonts/fonts.css"
+import BackgroundImage from "./BackgroundImage"
+import Header from "./Header"
+import StartButton from "./StartButton"
+import TextBox from "./TextBox"
+
+function StartPage() {
+
+  return (
+    <div>
+       <BackgroundImage />
+       <Header />
+       <TextBox />
+       <StartButton />
+    </div>
+  )
+}
+
+export default StartPage
