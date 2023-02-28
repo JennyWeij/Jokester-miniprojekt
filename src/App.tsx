15d397209c219a37a715f1f70152bcb644559787
JennyWeij <jennyweij@outlook.com>
2023-02-28 16:00:55 +0100

    hämtat main till min branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,17 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import "./assets/fonts/fonts.css"
-<<<<<<< HEAD
-
-import BackgroundImage from './BackgroundImage'
-import EmptyTextBox from './EmptyTextBox'
-import Footer from './Footer'
-=======
->>>>>>> origin/main
 import Header from './Header'
-import JokePageButtons from './JokePageButtons'
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main
 
 
 function App() {
 
   return (
     <div>
-<<<<<<< HEAD
-       <BackgroundImage />
-       <Header />
-      <JokePageButtons />
-       <Footer />
-
-    <EmptyTextBox />
-=======
       <Header />
     <Outlet />
 
->>>>>>> origin/main
 
     </div>
   )
