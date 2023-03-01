2b9fcd39fba474c2c3ae7b0772a8675c393cbbe0
Martina Kreivi <martinakreivi@gmail.com>
2023-03-01 14:00:41 +0100

    Nu funkar det men behöver styling

--- src/ShareButton.tsx
+++ src/ShareButton.tsx
@@ -1,17 +1,35 @@
-import styled from "styled-components"
+import { useState } from "react";
+import styled from "styled-components";
 
 function ShareButton() {
+  const [showInput, setShowInput] = useState(false);
+  
   const handleClick = () => {
+    setShowInput(true);
     console.log('Klickad')
+
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Skickat')
   }
+
   return (
     <>
-    <Button onClick={handleClick}>
-      <ImageShare src="src/assets/Share-Button.svg" />
+    {!showInput ? ( 
+      <Button onClick={handleClick}>
+        <ImageShare src="src/assets/Share-Button.svg" />
       </Button>
+    ) : (
+      <Form onSubmit={handleClick}>
+        <Input type="text" placeholder="Insert email here.."/>
+        <SubmitButton type="submit">Send</SubmitButton>
+      </Form>
+    )}
     </>
   )
 }
+
 const ImageShare = styled.img`
 width: 75px;`
 
@@ -20,6 +38,15 @@ cursor: pointer;
 border: none;
 background-color: transparent;`
 
+const Form = styled.form`
+display:flex;
+flex-direction: column;`
+
+const Input = styled.input`
+width: 70px;`
+
+const SubmitButton = styled.button`
+color: yellow;`
 
 
 export default ShareButton
