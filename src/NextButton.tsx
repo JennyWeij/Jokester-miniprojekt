4626807ccdde7db305306dc60c4da720c764a697
JennyWeij <jennyweij@outlook.com>
2023-03-01 14:12:42 +0100

    lagt next-button i en egen komponent, kopplat next-button till rendering av skämten

---
+++ src/NextButton.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+interface Props {
+  handleNewJoke: () => void;
+}
+
+function NextButton(props: Props) {
+  return (
+    <ButtonStart>
+      <ImageNext src="src/assets/NEXT-Button.svg" onClick={props.handleNewJoke} />
+    </ButtonStart>
+  )
+}
+const ImageNext = styled.img`
+cursor: pointer;
+width: 250px;
+`
+const ButtonStart = styled.div`
+position: absolute;
+top: 80%;
+left: 35%;
+
+`;
+
+export default NextButton
