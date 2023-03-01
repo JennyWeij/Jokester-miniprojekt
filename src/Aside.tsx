6beaa065146138624487b8047824134781f61c53
HaNNa89 <petersson.h@hotmail.com>
2023-03-01 10:04:34 +0100

    added new component, SidebarButtons + fixed links to Aside.

--- src/Aside.tsx
+++ src/Aside.tsx
@@ -1,48 +1,52 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import BackgroundImage from "./BackgroundImage";
 import Header from "./Header";
 
+
 function Aside(){
     return(
         <div>
-           <Header />
+            <Header />
+            <HeadingStyle>
+                <div>
+                <h4>Choose your category</h4>
+                </div>
+                
+                </HeadingStyle>
+          <BackgroundImage />
+        
            <SidebarStyle>
-           <HeadingStyle>
-           <div>
-           <h4>Choose joke by category</h4>
-           </div>
-           </HeadingStyle>
-           <aside>
-            <SideButtons>
-            Category 1
-            </SideButtons>
-            <SideButtons>
-            Category 2
-            </SideButtons>
-            <SideButtons>
-            Category 3
-            </SideButtons>
-            <SideButtons>
-            Category 4
-            </SideButtons>
-           </aside>
-           </SidebarStyle> 
-           <BackgroundImage />
+           
+            <aside>
+            <nav>
+                <ul>
+                    <li><SidebarLinks to="programming">Programming</SidebarLinks></li>
+                    <li><SidebarLinks to="general">General</SidebarLinks></li>
+                </ul>
+            </nav>
+            </aside>
+           
+           </SidebarStyle>
+          
         </div>
     );
 }
 
-const HeadingStyle = styled.h4`
-background-color: rgba(107, 8, 72, 0.5);
+const SidebarLinks = styled(Link)`
+background-color: rgba(107, 8, 72, 1);
 color: white;
+font-size: 24px;
 display: flex;
 flex-direction: column;
 justify-content: center;
-align-items: center;
-text-align: center;
-font-size: 20px;
-padding: 8px;
-font-family: Poppins-Bold;
+margin: 24px;
+padding: 10px 16px;
+list-style: none;
+text-decoration: none;
+border: none;
+border-radius: 40px;
+font-family: Poppins-Regular;
 `;
 
 const SidebarStyle = styled.aside`
@@ -50,27 +54,21 @@ position: absolute;
 display: flex;
 flex-direction: column;
 justify-content: center;
-align-items: center;
-text-align: center;
 background-color: rgba(107, 8, 72, 0.5);
 width: 20%;
 height: 100%;
-
 `;
 
-const SideButtons = styled.button`
-background-color: rgba(107, 8, 72, 1);
+const HeadingStyle = styled.div`
+background-color: rgba(107, 8, 72, 0.5);
 color: white;
-font-size: 24px;
-margin: 14px;
-padding: 12px 18px;
-border: none;
-border-radius: 40px;
-font-family: Poppins-Regular;
-
-
-
-
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
+align-items: flex-start ;
+font-size: 20px;
+padding: 8px;
+font-family: Poppins-Bold;
 `;
 
 export default Aside;
