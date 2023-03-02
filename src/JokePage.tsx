8a2f3d898bb38ed4925b16041e3dd3961921dac7
JennyWeij <jennyweij@outlook.com>
2023-03-02 20:23:03 +0100

    vi har header och footer på alla komponenter

--- src/JokePage.tsx
+++ src/JokePage.tsx
@@ -1,6 +1,7 @@
 import BackgroundImage from "./BackgroundImage";
 import EmptyTextBox from "./EmptyTextBox";
 import ErrorBoundary from "./ErrorBoundary";
+import Footer from "./Footer";
 import Header from "./Header";
 import JokePageButtons from "./JokePageButtons";
 
@@ -8,13 +9,13 @@ import JokePageButtons from "./JokePageButtons";
 function JokePage () {
   return (
     <div> 
-      <Header />
+      <Footer /> 
       <BackgroundImage />
+      <Header />
       <ErrorBoundary>
         <EmptyTextBox category="programming" />
         </ErrorBoundary>
       <JokePageButtons />
-      {/* <Footer /> */}
     </div>
   )
 }
