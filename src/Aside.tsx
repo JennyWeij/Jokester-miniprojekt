dc031309fb033bc0aed46cc543941e66310783fa
JennyWeij <jennyweij@outlook.com>
2023-03-02 14:00:11 +0100

    lagt in rätt font och uppdaterat styling

--- src/Aside.tsx
+++ src/Aside.tsx
@@ -29,42 +29,43 @@ function Aside(){
 }
 
 const SidebarLinks = styled(Link)`
-background-color: rgba(107, 8, 72, 1);
-color: white;
-font-size: 24px;
-display: flex;
-flex-direction: column;
-justify-content: center;
-margin: 24px;
-padding: 10px 14px;
-list-style: none;
-text-decoration: none;
-border: none;
-border-radius: 40px;
-font-family: Poppins-Regular;
+  background-color: rgba(107, 8, 72, 1);
+  color: white;
+  font-size: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 24px;
+  padding: 10px 14px;
+  list-style: none;
+  text-decoration: none;
+  border: none;
+  border-radius: 40px;
+  font-family: PoppinsRegular;
 `;
 
 const SidebarStyle = styled.aside`
-position: absolute;
-display: flex;
-flex-direction: column;
-justify-content: center;
-background-color: rgba(107, 8, 72, 0.5);
-width: 16%;
-height: 100%;
+  position: absolute;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(107, 8, 72, 0.5);
+  width: 16%;
+  height: 100%;
 `;
 
 const HeadingStyle = styled.div`
-background-color: rgba(107, 8, 72, 0.5);
-color: white;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-align-items: center ;
-text-align: center;
-font-size: 20px;
-padding: 18px;
-font-family: Poppins-Regular;
+  background-color: rgba(107, 8, 72, 0.5);
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center ;
+  text-align: center;
+  font-size: 20px;
+  padding: 18px;
+  font-family: PoppinsRegular;
+  margin-top: 0;
 `;
 
 export default Aside;
