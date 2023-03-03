8824a13cc2d88048a209e203bcfcbd07c851bbe2
JennyWeij <jennyweij@outlook.com>
2023-03-03 13:38:42 +0100

    lagt till 404 page not found

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import App from './App'
 import GeneralJokesPage from './GeneralJokesPage'
-
 import JokePage from './JokePage'
 import MenuPage from './MenuPage'
 import StartPage from './StartPage'
@@ -16,6 +15,7 @@ const router = createBrowserRouter(
       <Route path="menu" element={<MenuPage />} />
       <Route path="/joke" element={<JokePage />} />
       <Route path="/generaljoke" element={<GeneralJokesPage />} />
+      <Route path="*" element={<h2> 404 page not found</h2>} />      
     </Route>
   )
 );
